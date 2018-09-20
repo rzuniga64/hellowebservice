da36549f70261cfba4055138533b3da2d7c5419e
Raul Zuniga <rzuniga64@gmail.com>
2018-09-19 22:55:39 -0500

    Reconfigure file structure.

--- src/main/java/com/ws/soap/config/WebServiceConfig.java
+++ src/main/java/com/ws/soap/config/WebServiceConfig.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.ws.soap.hellowebservice.HelloWS;
+import com.ws.soap.HelloWS;
 
 @Configuration
 public class WebServiceConfig {
