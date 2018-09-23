f626602b89ea91d6d06af5dabce2f9f2d338d748
Raul Zuniga <rzuniga64@gmail.com>
2018-09-23 01:33:23 -0500

    Add CXF logging feature.

---
+++ src/main/java/com/ws/soap/HelloWS.java
@@ -0,0 +1,15 @@
+package com.ws.soap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import org.apache.cxf.feature.Features;
+
+@WebService
+@Features(features = "org.apache.cxf.feature.LoggingFeature")
+public class HelloWS {
+
+    @WebMethod
+    public String hello() {
+        return "Hello";
+    }
+}
